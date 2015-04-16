e6d00136111caf87d6f88cddc4a41f8e6a7e1afe
Eric Snow <ericsnowcurrently@gmail.com>
2015-04-16 11:58:27 +0200

    Add a test and fix some bugs.

--- trusty/influxdb-injecter/files/server.py
+++ trusty/influxdb-injecter/files/server.py
@@ -3,6 +3,7 @@
 import os
 import socket
 import SocketServer as ss
+import threading
 import time
  
 
@@ -11,9 +12,14 @@ def send(host, port, msg):
     sock.connect((host, port))
     try:
         sock.sendall(msg)
+        result = sock.recv(1024)
         time.sleep(1)  # XXX why?
     finally:
         sock.close()
+
+    if result.startswith('ERROR: '):
+        raise Exception(result[7:])
+    return result
 
 
 class DataStore(object):
@@ -33,13 +39,13 @@ class DataStore(object):
 
         if self.verbose:
             print('sending message:\n' + msg)
-        send(self.host, self.port, msg, nsname)
+        send(self.host, self.port, msg)
 
 
 class _Handler(ss.BaseRequestHandler):
 
     def __init__(self, request, client_address, server):
-        super(_Handler, self).__init__(request, client_address, server)
+        ss.BaseRequestHandler.__init__(self, request, client_address, server)
         self.store = server.store
 
     def _parse(self):
@@ -51,13 +57,20 @@ class _Handler(ss.BaseRequestHandler):
         except ValueError:
             raise Exception('expected a number, got {!r}'.format(number))
 
+        # XXX Enforce bounds?
+
+        if not nsname:
+            nsname = None
         return number, nsname
 
     def handle(self):
+        #verbose = False
+        verbose = True
         try:
             number, nsname = self._parse()
-            self.store.store_data(number, nsname=nsname or None)
+            self.store.store_data(number, nsname=nsname, verbose=verbose)
         except Exception as e:
+            #raise
             msg = 'ERROR: {}'.format(e).decode('utf-8')
         else:
             msg = ''  # success
@@ -68,9 +81,13 @@ class _Handler(ss.BaseRequestHandler):
 class Server(ss.TCPServer):
 
     def __init__(self, host, port, store):
-        super(Server, self).__init__((host, port), self._handler)
+        print(host, port)
+        ss.TCPServer.__init__(self, (host, port), self._handler)
 
         self.store = store
+
+        self.thread = threading.Thread(target=self.serve_forever)
+        self.thread.daemon = True
 
     def _handler(self, request, client_address, server):
         return _Handler(request, client_address, server)
@@ -79,11 +96,7 @@ class Server(ss.TCPServer):
         self.serve_forever()
 
     def run_background(self):
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-
-        #server.shutdown()
+        self.thread.start()
 
     def pid(self):
         return os.getpid()
@@ -105,6 +118,7 @@ def main(targethost, targetport, host, port, ns):
     print(server.pid())
     #server.run()
     server.run_background()
+    return server
 
 
 if __name__ == '__main__':
@@ -117,13 +131,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default=DEFAULT_HOST,
                         help='the address of the data store server')
-    parser.add_argument('--port', default=DEFAULT_PORT,
+    parser.add_argument('--port', type=int, default=DEFAULT_PORT,
                         help='the port of the data store server')
     parser.add_argument('--ns', default=DEFAULT_NAMESPACE,
                         help='the DB namespace to target')
     parser.add_argument('targethost',
                         help='the address of the data store server')
-    parser.add_argument('targetport',
+    parser.add_argument('targetport', type=int,
                         help='the port of the data store server')
     args = parser.parse_args()
 
---
+++ trusty/influxdb-injecter/files/test_server.py
@@ -0,0 +1,93 @@
+import random
+#import socket
+import SocketServer as ss
+import unittest
+
+import server
+
+
+HOST = '127.0.0.1'
+TPORT = 9998
+PORT = 9999
+NS = 'test'
+
+
+class FakeHandler(ss.BaseRequestHandler):
+
+    def handle(self):
+        self.msg = self.request.recv(1024).encode('utf-8')
+
+
+class BaseSuite(unittest.TestCase):
+
+    def setUp(self):
+        super(BaseSuite, self).setUp()
+
+        self.handlers = []
+        self.sent = []
+
+        self.numbers = [random.randint(1, 20) for _ in range(9)]
+
+    @property
+    def handler(self):
+        try:
+            return self.handlers[-1]
+        except IndexError:
+            return None
+
+    def _new_handler(self, *args, **kwargs):
+        handler = FakeHandler(*args, **kwargs)
+        self.handlers.append(handler)
+        return handler
+
+    def new_server(self, port):
+        return ss.TCPServer((HOST, port), self._new_handler)
+
+    def send(self, host, port, data):
+        self.sent.append((host, port, data))
+
+
+class TestDataStore(BaseSuite):
+
+    def test_stuff(self):
+        pass
+
+
+class TestServer(BaseSuite):
+
+    def test_run(self):
+        pass
+
+    def test_run_background(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestMain(BaseSuite):
+
+    def test_okay(self):
+        send = server.send
+        server.send = self.send
+        try:
+            srv = server.main(HOST, TPORT, HOST, PORT, NS)
+            send(HOST, PORT, b'10')
+            srv.shutdown()
+        finally:
+            server.send = send
+
+        self.assertEqual(len(self.sent), 1)
+        sent, = self.sent
+        host, port, msg = sent
+        self.assertEqual(host, HOST)
+        self.assertEqual(port, TPORT)
+
+        series, number, timestamp = msg.encode('utf-8').split()
+        self.assertEqual(series, 'test')
+        self.assertEqual(number, '10')
+        self.assertEqual(timestamp, '')
+
+
+if __name__ == '__main__':
+    unittest.main()
